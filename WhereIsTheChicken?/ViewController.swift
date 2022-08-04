c7e536acdad83ee32355f52d4f4c9c42c846d8de
Dusan-O <dusan.orescanin@gmail.com>
2022-08-04 23:15:33 +0200

    Add play() & stop() functionalities

--- WhereIsTheChicken?/ViewController.swift
+++ WhereIsTheChicken?/ViewController.swift
@@ -18,16 +18,51 @@ class ViewController: UIViewController {
     @IBOutlet weak var yesBtn: UIButton!
     @IBOutlet weak var noBtn: UIButton!
     
+    var isGameOn = false
+    var score: Int = 0
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
 
     }
+    
+    func play() {
+        isGameOn = true
+        updateUI()
+    }
+    
+    func stop() {
+        isGameOn = false
+        updateUI()
+    }
+    
+    func checkAnswer(taf : Int) {
+
+    }
+    
+    func updateUI() {
+        yesBtn.isHidden = !isGameOn
+        noBtn.isHidden = !isGameOn
+        playBtn.setTitle(!isGameOn ? "Play" : "Stop", for: .normal)
+        resultLabel?.isHidden = !isGameOn
+    }
+    
+    func updateScore() {
+        scoreLbl.text = "Score: \(score)"
+    }
+    
+    func updateAnswer(success: Bool) {
+        resultLabel.text = success ? "Gagné" : "Perdu"
+    }
+    
     
     @IBAction func playBtnPressed(_ sender: Any) {
+        isGameOn ? stop() : play()
     }
     
-
     @IBAction func answerPressed(_ sender: UIButton) {
     }
     
