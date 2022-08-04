69281aca9ef52d776f4b4efac572f3ff94f537ca
Dusan-O <dusan.orescanin@gmail.com>
2022-08-04 22:35:20 +0200

    Update of the app UI + IBOutlets + IBActions connected

--- WhereIsTheChicken?/ViewController.swift
+++ WhereIsTheChicken?/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playBtn: UIButton!
+    
+    
+    @IBOutlet weak var scoreLbl: UILabel!
+    @IBOutlet weak var chickenIV: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var yesBtn: UIButton!
+    @IBOutlet weak var noBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
+    
+    @IBAction func playBtnPressed(_ sender: Any) {
+    }
+    
+
+    @IBAction func answerPressed(_ sender: UIButton) {
+    }
+    
 
 }
 
